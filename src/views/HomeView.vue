1cf20f5336d631d6d8c0a860881627a954eeb9cf
LiuHN <1465638317@qq.com>
2023-10-26 13:05:21 +0800

    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home"></div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="less">
+  .home {
+    height: 100vh;
+    width: 100%;
+    background: url('../assets/img/login_bg.webp') no-repeat;
+    background-size: cover;
+  }
+</style>
