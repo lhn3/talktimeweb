72d7b3a1843996498d077d565ded122ca4047fd9
LiuHN <1465638317@qq.com>
2023-10-26 15:32:39 +0800

    feat: 新增提交规范

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 <script setup lang="ts"></script>
 <style scoped lang="less">
   .home {
-    height: 100vh;
+    height: 100%;
     width: 100%;
     background: url('../assets/img/login_bg.webp') no-repeat;
     background-size: cover;
